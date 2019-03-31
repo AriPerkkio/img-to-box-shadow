db93006f1181bcfaaa6894e47c54a48744cfdf1d
Ari Perkkio <ari.perkkio@gmail.com>
2019-03-31 16:05:56 +0000

    Index as Module + test added

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-const fs = require("fs");
 const PNG = require("png-js");
-
-const fileName = "resources/colors.png";
-const width = 500;
-const height = 500;
-const ratio = 1;
 
 const decToHex = dec => {
     const hex = dec.toString(16);
@@ -12,38 +6,46 @@ const decToHex = dec => {
     return hex.length === 1 ? `0${hex}` : hex;
 };
 
-const getRows = data => {
-    const rows = [];
+const getRows = (data, options) => {
+    const { width, ratio } = options;
 
-    for (let i = 0; i < data.length; i += width * 4) {
-        const row = data.slice(i, i + width * 4);
-        const rgbas = [];
+    const rows = [];
+    const rowSliceLength = width * 4;
+
+    for (let i = 0; i < data.length; i += rowSliceLength) {
+        const row = data.slice(i, i + rowSliceLength);
+        const cells = [];
 
         for (let x = 0; x < row.length; x += 4) {
-            const [r, g, b, a] = row.slice(x, x + 4);
+            const [r, g, b] = row.slice(x, x + 4);
+            const hexColor = `#${decToHex(r)}${decToHex(g)}${decToHex(b)}`;
 
-            rgbas.push(
-                `${(x / 4) * ratio}px ${rows.length * ratio}px 0 #${decToHex(r)}${decToHex(g)}${decToHex(b)}`
+            cells.push(
+                `${(x / 4) * ratio}px ${rows.length * ratio}px 0 ${hexColor}`
             );
         }
 
-        rows.push(rgbas.join(", "));
+        rows.push(cells.join(", "));
     }
 
     return rows;
 };
 
+const PngToBoxShadow = (options = {}, callback) => {
+    try {
+        PNG.decode(options.fileName, data => {
+            try {
+                const rows = getRows(data, options);
+                const output = rows.join(",\n");
 
-PNG.decode(fileName, pixels => {
-    const rows = getRows(pixels);
-
-    const output = `    #box-shadow-tester {
-        height: ${ratio}px;
-        width: ${ratio}px;
-        box-shadow:
-            ${rows.join(",\n")};
+                callback(null, output);
+            } catch (e) {
+                callback(e);
+            }
+        });
+    } catch (e) {
+        callback(e);
     }
-`;
+};
 
-    fs.writeFile("main.css", output, "utf8", () => { });
-});
+module.exports = PngToBoxShadow;
