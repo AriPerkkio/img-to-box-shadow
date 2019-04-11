e3b355dba8b82c7fbb53decd98a5f2d27d79e7a7
AriPerkkio <ari.perkkio@gmail.com>
2019-04-11 15:53:32 +0000

    MergeVertical initial implementation

--- src/__tests__/merge.test.js
+++ src/__tests__/merge.test.js
@@ -1,17 +1,26 @@
-const { mergeHorizontal } = require('../merge');
+const { mergeHorizontal, mergeVertical } = require('../merge');
+
+const colorOne = 'colorOne';
+const colorTwo = 'colorTwo';
 
 const cell = (x, y, rgba) => ({ x, y, rgba });
 const cells = [
-    cell(0, 0, 'rgba1'),
-    cell(1, 0, 'rgba1'),
-    cell(2, 0, 'rgba2'),
-    cell(3, 0, 'rgba1'),
-    cell(4, 0, 'rgba1'),
-    cell(5, 0, 'rgba1'),
-    cell(6, 0, 'rgba1'),
-    cell(7, 0, 'rgba2'),
-    cell(8, 0, 'rgba2'),
-    cell(9, 0, 'rgba2'),
+    cell(0, 0, colorOne),
+    cell(1, 0, colorOne),
+    cell(2, 0, colorTwo),
+    cell(3, 0, colorOne),
+    cell(4, 0, colorOne),
+    cell(5, 0, colorOne),
+    cell(6, 0, colorOne),
+    cell(7, 0, colorTwo),
+    cell(8, 0, colorTwo),
+    cell(9, 0, colorTwo),
+];
+
+const rows = [
+    [cell(0, 0, colorOne), cell(1, 0, colorOne)],
+    [cell(0, 1, colorTwo), cell(1, 1, colorOne)],
+    [cell(0, 2, colorTwo), cell(1, 2, colorTwo)],
 ];
 
 describe('Merge', () => {
@@ -20,17 +29,23 @@ describe('Merge', () => {
             const mergedCells = mergeHorizontal(cells);
 
             expect(mergedCells).toEqual([
-                cell(1, 0, 'rgba1'),
-                cell(2, 0, 'rgba2'),
-                cell(6, 0, 'rgba1'),
-                cell(9, 0, 'rgba2'),
+                cell(1, 0, colorOne),
+                cell(2, 0, colorTwo),
+                cell(6, 0, colorOne),
+                cell(9, 0, colorTwo),
             ]);
         });
     });
 
     describe('mergeVertical', () => {
-        it('should...', () => {
-            expect(true).toBe(true);
+        it('should merge cells vertically', () => {
+            const mergedRows = mergeVertical(rows);
+
+            expect(mergedRows).toEqual([
+                [cell(0, 0, colorOne), cell(1, 1, colorOne)],
+                [cell(0, 2, colorTwo), null],
+                [null, cell(1, 2, colorTwo)],
+            ]);
         });
     });
 });
--- src/merge.js
+++ src/merge.js
@@ -14,8 +14,7 @@ const mergeHorizontal = cells => {
 
         let x = 0 + left.x;
         while (y < lastIndex && color === cells[y + 1].rgba) {
-            y++;
-            x++;
+            y++, x++;
         }
 
         mergedCells.push({ ...left, x });
@@ -24,7 +23,63 @@ const mergeHorizontal = cells => {
     return mergedCells;
 };
 
-const mergeVertical = rows => rows;
+const mergeVertical = rows => {
+    const maxRowLength = Math.max(...rows.map(r => r.length));
+    const mergedRows = createEmptyClone(rows);
+    const lastIndex = rows.length - 1;
+
+    for (let x = 0; x < maxRowLength; x++) {
+        for (let y = 0; y < rows.length; y++) {
+            const upper = rows[y][x];
+            const color = upper.rgba;
+            const isLast = y === lastIndex;
+            const initialY = 0 + y;
+
+            if (isLast || color != rows[y + 1][x].rgba) {
+                mergedRows[y][x] = { ...upper };
+                continue;
+            }
+
+            let upperY = 0 + upper.y;
+            while (y < lastIndex && color === rows[y + 1][x].rgba) {
+                mergedRows[y][x] = null;
+                y++, upperY++;
+            }
+
+            mergedRows[initialY][x] = { ...upper, y: upperY };
+        }
+    }
+
+    return mergedRows;
+};
+
+const createEmptyClone = array =>
+    JSON.parse(
+        JSON.stringify(
+            new Array(array.length).fill(
+                new Array(Math.max(...array.map(r => r.length)))
+            )
+        )
+    );
+
+const debugRows = (rows, label = '') => {
+    const printRows = createEmptyClone(rows);
+
+    rows.map((row, x) =>
+        row.map((cell, y) => {
+            printRows[x][y] = cell && cell.rgba;
+        })
+    );
+
+    const output = `${label}
+            \n${printRows
+                .map(row => row.map(color => `[${color}]`).join('\t'))
+                .map((colors, y) => `Y${y}\t${colors}`)
+                .join('\n')}`;
+
+    console.log(output);
+    return output;
+};
 
 module.exports = {
     mergeHorizontal,
